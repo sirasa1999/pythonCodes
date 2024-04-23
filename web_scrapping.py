b9a14240b67a9b33b20a645ae46b219befa723a5
Sirasa Tuladhar <72450330+sirasa1999@users.noreply.github.com>
2024-04-23 21:37:38 +0545

    new webscrapping file uploaded

---
+++ web_scrapping.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+def cnn_headlines_func(url):
+    """ This function gets request from CNN homepage, scraps using BeautifulSoup
+      and returns its headline and description"""
+    try:
+        response = requests.get(url)
+        print(response.status_code)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            print(soup)
+            headlines = [headline.text.strip() for headline in soup.find_all('item')]
+            descriptions = [description.text.strip() for description in soup.find_all('item')]
+            return headlines, descriptions
+        else:
+            return print("CNN data extraction failure,", response.status_code)
+    except Exception as e:
+        return print("Error occured:", e)
+
+def save_to_csv(headlines, descriptions, file_name):
+    """ This function stores the healines and description in csv file"""
+    try:
+        with open(file_name, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Headline', 'description'])
+            for headline, description in zip(headlines, descriptions):
+                writer.writerow([headline, description])
+        print("Data saved to", file_name)
+    except Exception as e:
+        print("Error saving data to CSV:", e)
+
+
+cnn_urls_and_filenames = {
+    'http://rss.cnn.com/rss/cnn_topstories.rss': 'cnn_topstories.csv',
+    'http://rss.cnn.com/rss/cnn_us.rss': 'cnn_us.csv'
+}
+# Loop through each CNN feed URL and filename pair, scrape headlines and descriptions, and save them to CSV
+for cnn_url, file_name in cnn_urls_and_filenames.items():
+    headlines, descriptions = cnn_headlines_func(cnn_url)
+    if headlines and descriptions:
+        save_to_csv(headlines, descriptions, file_name)
